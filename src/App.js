a6868f4dc1614d482b1c99f22bbf698a601d5746
balachandar <balachandark.bsf.io>
2020-01-06 00:23:25 +0530

    added a todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItems: ['Learn React', 'Learn TypeScript'],
+      itemName: ''
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      itemName: e.target.value
+    })
+  }
+
+  clickHandler = (e) => {
+    console.log( e.target )
+    this.setState( prevState => {
+      const prevTodoItems = prevState.todoItems;
+      prevTodoItems.push( prevState.itemName );
+      return ({
+        todoItems: prevTodoItems,
+      })
+    })
+  }
+
+  handleKeyDown = (e) => {
+    if( e.key === 'Enter' ) {
+      this.setState( prevState => {
+        const prevTodoItems = prevState.todoItems;
+        prevTodoItems.push( prevState.itemName );
+        return ({
+          todoItems: prevTodoItems,
+        })
+      })
+    }
+  }
+
+  closeHandler = (e) => {
+    console.log( e );
+    console.log( e.target );
+    console.log( e.relatedTarget );
+  }
+
+  render() {
+    const todoItems = this.state.todoItems;
+    return (
+      <div className="App">
+        <h1>To-Do List</h1>
+        <input type="text" className="addNewItem" placeholder="Add a new todo list ..."onKeyDown={this.handleKeyDown} onChange={this.changeHandler}/>
+        <button className="addButton" onClick={this.clickHandler}>Add</button>
+        <ul className="list-group">
+          {
+            todoItems.map( (currentItem, index ) => {
+              return (
+                <li className="list-item" key={index}>
+                  <div className="item">{currentItem}</div>
+                  <span type="button" className="close" onClick={this.closeHandler}>×</span>
+                </li>
+              ) 
+            } )
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
