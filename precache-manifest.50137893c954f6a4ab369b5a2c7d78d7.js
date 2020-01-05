1380b1ac26221b1567e424929f5820446df0a24d
balachandar <balachandark.bsf.io>
2020-01-06 00:27:32 +0530

    Updates

---
+++ precache-manifest.50137893c954f6a4ab369b5a2c7d78d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34583b434536f9374ab00cacad70c29a",
+    "url": "/simple-to-do-list/index.html"
+  },
+  {
+    "revision": "092a59dafb02e3544653",
+    "url": "/simple-to-do-list/static/css/main.e6ab9f70.chunk.css"
+  },
+  {
+    "revision": "09ab6d882de4613875fa",
+    "url": "/simple-to-do-list/static/js/2.cfeabcf9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/simple-to-do-list/static/js/2.cfeabcf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "092a59dafb02e3544653",
+    "url": "/simple-to-do-list/static/js/main.51ba1c66.chunk.js"
+  },
+  {
+    "revision": "9f4de6b5d1c8a4496c06",
+    "url": "/simple-to-do-list/static/js/runtime-main.e57bcb68.js"
+  }
+]);
